1fa72c52aa45c4a6a1a59b759ad0689c629661dd
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-01-18 12:34:04 +0530

    feat: add total score calculation

---
+++ bowlingGame.js
@@ -0,0 +1,32 @@
+const  score = (rolls) => {
+    let len = rolls.length
+    let index=0;
+    let result =0,counter=0;
+    while(index<len && counter<10) {
+        if((rolls[index]+rolls[index+1])<10) {
+            result += rolls[index]+rolls[index+1];
+            counter+=1;
+        }
+        else if(rolls[index]+rolls[index+1]==10) {
+            result += rolls[index]+rolls[index+1]+rolls[index+2];
+            counter+=1;
+        }
+        else {
+            if(rolls[index]==10) {
+            result+= rolls[index]+rolls[index+1]+rolls[index+2];
+            counter+=1;
+            }
+        }
+        index+=2;
+    }
+return result;
+}
+
+// console.log(score([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]));
+// console.log(score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]));
+// console.log(score([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
+// console.log(score([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]));
+
+module.exports = {
+    score:score
+}
